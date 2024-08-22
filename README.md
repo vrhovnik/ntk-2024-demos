0f7469c2abbf3684733c7e2bbe1ecc279147d7bf
Bojan Vrhovnik <bojan@vrhovnik.net>
2024-08-22 14:44:09 +0200

    Adding initial files for the NTK 24 demo repository

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# ntk-2024-demos
-NTK 2024 demos and additional information
+# NTK 2024 - demo repository
+
+Demos for the [NTK 2024](https://www.ntk.si) session Container Options in Azure (
+more [here](https://www.ntk.si/urnik/predavanje/uporaba_kontajnerjev_v_platformi/51) ).
+
+To explore different approaches for launching and managing containers in the Azure platform (Azure
+Container Apps, Azure Kubernetes Services, Azure Container Instances, Azure Web App for Containers, etc.), compare them
+and show their advantages and disadvantages, ways of integration into other information systems and use for easy
+installation, maintenance and management according to the number of customers and service requirements, I focus on:
+
+1. **Demo applications** - simple web applications that we will deploy to the Azure platform using different container
+   options.
+2. **Demo APIs** - simple APIs that we will deploy to the Azure platform using different container options. Majority of
+   the api will be privately
+   available only to the web application (with few public options access availability).
+3. **Demo databases** - simple databases that we will deploy to the Azure platform using different container options.
+4. **Scripts** - scripts for easy deployment and management of the above.
+
+## Prerequisites
+
+1. An active [Azure](https://www.azure.com) subscription - [MSDN](https://my.visualstudio.com) or trial
+   or [Azure Pass](https://microsoftazurepass.com) is fine - you can also do all of the work
+   in [Azure Shell](https://shell.azure.com) (all tools installed) and by
+   using [Github Codespaces](https://docs.github.com/en/codespaces/developing-in-codespaces/creating-a-codespace)
+2. [Azure CLI](https://learn.microsoft.com/en-us/cli/azure/) installed to work with Azure
+3. [GitHub](https://github.com/) account (sign-in or join [here](https://github.com/join)) - how to authenticate with
+   GitHub
+   available [here](https://docs.github.com/en/get-started/quickstart/set-up-git#authenticating-with-github-from-git)
+4. [RECOMMENDATION] [PowerShell](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.2)
+   installed - we do recommend an editor like [Visual Studio Code](https://code.visualstudio.com) to be able to write
+   scripts, YAML pipelines and connect to repos to submit changes.
+5. [OPTIONAL] GitHub CLI installed to work with GitHub - [how to install](https://cli.github.com/manual/installation)
+6. [OPTIONAL] [Github GUI App](https://desktop.github.com/) for managing changes and work
+   on [forked](https://docs.github.com/en/get-started/quickstart/fork-a-repo) repo
+7. [OPTIONAL] [Windows Terminal](https://learn.microsoft.com/en-us/windows/terminal/install)
+
+If you will be working on your local machines, you will need to have:
+
+1. [Powershell](https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.2)
+   installed
+2. git installed - instructions step by step [here](https://docs.github.com/en/get-started/quickstart/set-up-git)
+3. [.NET](https://dot.net) installed to run the application if you want to run it
+4. an editor (besides notepad) to see and work with code, yaml, scripts and more (for
+   example [Visual Studio Code](https://code.visualstudio.com))
+
+## Scripts
+
+Scripts are available in [scripts folder](./scripts). The scripts are written
+in [PowerShell](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.2).
+
+1. [Add-DirToSystemEnv.ps1](./scripts/Add-DirToSystemEnv.ps1) - adds a directory to the system environment variable
+   PATH
+2. [Install-AZCLI.ps1](./scripts/Install-AZCLI.ps1) - installs Azure CLI
+3. [Install-Bicep.ps1](./scripts/Install-Bicep.ps1) - installs Bicep language
+
+## Demo applications
+
+TBD
+
+## Demo APIs
+
+TBD
+
+## Demo databases
+
+TBD
+
+## Scripts
+
+Scripts to help deploying to the cloud and working with demos are available in the `scripts` directory. They are written
+in PowerShell and use Bicep do deploy infrastructure as code and to help with applications.
+
+[Docker files](./containers) are available to build and run the application in containers. You can also leverage helper
+script [Compile-Containers.ps1](./scripts/Compile-Containers.ps1) to build containers
+using [Azure Container Registry task builders](https://learn.microsoft.com/en-us/azure/container-registry/container-registry-tutorial-build-task).
+
+# Links and additional information
+
+1. [NTK 2024](https://www.ntk.si)
+2. [Azure Portal](https://portal.azure.com/)
+3. [Azure Container Apps](https://azure.microsoft.com/en-us/services/container-apps/)
+4. [Azure Kubernetes Services](https://azure.microsoft.com/en-us/services/kubernetes-service/)
+5. [Azure Container Instances](https://azure.microsoft.com/en-us/services/container-instances/)
+6. [Azure Web App for Containers](https://azure.microsoft.com/en-us/services/app-service/containers/)
---
+++ scripts/Init-Environment.ps1
@@ -0,0 +1,152 @@
+﻿<#
+
+# .SYNOPSIS
+
+Install all required resources for Azure 2 Sharepoint demo from scratch.
+
+.DESCRIPTION
+ 
+This script will install all required resources for Azure 2 Sharepoint demo from scratch
+with the use of Bicep templates. It will also install Bicep and Azure CLI if selected and get environment 
+variables from local.env file. 
+  
+.EXAMPLE
+
+PS > Init-Environment -Location "WestEurope" -InstallModules -InstallBicep -UseEnvFile
+install modules, bicep and deploy resources to West Europe
+
+.EXAMPLE
+
+PS > Init-Environment  -Location "WestEurope" -OpenLogFile
+install all required resources to West Europe and open log file with notepad with detailed data
+ 
+.EXAMPLE
+
+PS > Init-Environment  -Location "WestEurope" -OpenLogFile -OpenBrowserWithLatestUrl
+install all required resources to West Europe and open log file with notepad with detailed data and the latest url in browser
+ 
+. LINK
+
+http://github.com/bovrhovn
+ 
+#>
+param(
+    [Parameter(Mandatory = $false)]
+    $Location = "SwedenCentral",
+    [Parameter(Mandatory = $false)]
+    $EnvFileToReadFrom = "local.env",
+    [Parameter(Mandatory = $false)]
+    [switch]$InstallModules,
+    [Parameter(Mandatory = $false)]
+    [switch]$InstallBicep,
+    [Parameter(Mandatory = $false)]
+    [switch]$InstallAzCli,
+    [Parameter(Mandatory = $false)]
+    [switch]$UseEnvFile,   
+    [Parameter(Mandatory = $false)]
+    [switch]$LoginToAzure, 
+    [Parameter(Mandatory = $false)]
+    [switch]$OpenLogFile,
+    [Parameter(Mandatory = $false)]
+    [switch]$OpenBrowserWithLatestUrl   
+)
+
+$ProgressPreference = 'SilentlyContinue';
+Start-Transcript -Path "$HOME/Downloads/bootstrapper.log" -Force
+# Write-Output "Sign in to Azure account." 
+# login to Azure account if not logged in
+
+if ($LoginToAzure)
+{
+    Write-Output "Sign in to Azure account." 
+    Connect-AzAccount
+}
+
+if ($InstallModules)
+{
+    Write-Output "Install Az module and register providers."
+    #install Az module
+    Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+    Install-Module -Name Az.App
+
+    #register providers
+    Register-AzResourceProvider -ProviderNamespace Microsoft.App
+    # add support for log analytics
+    Register-AzResourceProvider -ProviderNamespace Microsoft.OperationalInsights
+    Write-Output "Modules installed and registered, continuing to Azure deployment nad if selected, Bicep install."
+}
+
+if ($InstallBicep)
+{
+    # install bicep
+    Write-Output "Installing Bicep."
+    # & Install-Bicep.ps1
+    Start-Process powershell.exe -FilePath Install-Bicep.ps1 -NoNewWindow -Wait
+    Write-Output "Bicep installed, continuing to Azure deployment."
+}
+
+if ($InstallAzCli)
+{
+    Write-Output "Installing Azure CLI."
+
+    Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi;
+    Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet';
+    ## cleanup of Azure CLI
+    Remove-Item .\AzureCLI.msi
+    Write-Output "Az CLI installed, continuing to Azure deployment."
+}
+if ($UseEnvFile)
+{
+    Get-Content $EnvFileToReadFrom | ForEach-Object {
+        $name, $value = $_.split('=')
+        Set-Content env:\$name $value
+        Write-Information "Writing $name to environment variable with $value."
+    }
+}
+
+# create resource group if it doesn't exist with bicep file stored in bicep folder
+$groupNameExport = New-AzSubscriptionDeployment -Location $Location -TemplateFile "..\Bicep\rg.bicep" -TemplateParameterFile "..\Bicep\rg.parameters.json" -Verbose
+
+Write-Verbose "Resource group created $groupNameExport"
+$groupName = $groupNameExport.Outputs.rgName.value
+Write-Output "The resource group name is $groupName"
+
+# deploy web app with application insights
+$sqlDetails = New-AzResourceGroupDeployment -ResourceGroupName $groupName -TemplateFile "..\Bicep\sql.bicep" -TemplateParameterFile "..\Bicep\sql.parameters.json" -Verbose
+Write-Output "SQL deployed to $Location with details $sqlDetails"
+$appSettings = Get-Content -Path "..\Bicep\sql.parameters.json" | ConvertFrom-Json
+$serverName=$appSettings.parameters.serverName.value
+$sqlDBName=$appSettings.parameters.sqlDBName.value
+$administratorLogin=$appSettings.parameters.administratorLogin.value
+$administratorLoginPassword=$appSettings.parameters.administratorLoginPassword.value
+Write-Information "Reading data from sql.parameters.json $sqlDBName and $serverName and $administratorLogin and $administratorLoginPassword."
+$sqlConnString = "Server=tcp:$serverName.database.windows.net,1433;Initial Catalog=$sqlDBName;Persist Security Info=False;User ID=$administratorLogin@mcm-data-server.database.windows.net;Password=$administratorLoginPassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+
+$appSettings = Get-Content -Path "..\Bicep\webapp.parameters.json" | ConvertFrom-Json
+Write-Verbose "Getting app settings from webapp.parameters.json $appSettings"
+Write-Output "SQL connection string to set is $sqlConnString"
+Write-Verbose "Updating connection string setting for web app."
+$appSettings.parameters.sqlConn.value=$sqlConnString
+Write-Verbose "Updating app settings with connection string $sqlConnString"
+Set-Content -Path "..\Bicep\webapp.parameters.json" -Value ($appSettings | ConvertTo-Json)
+Write-Output "Connection string updated for web app. Deploying web app."
+
+$urlDetails = New-AzResourceGroupDeployment -ResourceGroupName $groupName -TemplateFile "..\Bicep\webapp.bicep" -TemplateParameterFile "..\Bicep\webapp.parameters.json" -Verbose
+$urlToOpen = $sqlDetails.Outputs.pageUrl.value
+if (-not [string]::IsNullOrEmpty($urlToOpen))
+{
+    Write-Output "Web App deployed to $Location with $urlToOpen"
+}
+Stop-Transcript
+
+if ($OpenBrowserWithLatestUrl)
+{
+    # open browser with latest url
+    Start-Process $urlToOpen
+}
+
+if ($OpenLogFile)
+{
+    # open file for viewing
+    Start-Process notepad.exe -ArgumentList "$HOME/Downloads/bootstrapper.log"
+}
